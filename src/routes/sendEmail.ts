fc2b65c400aac78f78ddae8a4602e973c831b26c
xakxa01 <Carlosxaviergomez@outlook.com>
2024-07-22 14:26:39 -0400

    add hello world in get

--- src/routes/sendEmail.ts
+++ src/routes/sendEmail.ts
@@ -9,28 +9,30 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 const router = Router()
 
-router.post("/", async (req: Request, res: Response) => {
-	const body = req.body
+router
+	.get('/', (req, res) => res.send('hello world'))
+	.post("/", async (req: Request, res: Response) => {
+		const body = req.body
 
-	const { attachments } = body;
-	const title = 'Ayuda Concho'
-	const subject = 'Report'
-	const html = htmlTemplate(body)
+		const { attachments } = body;
+		const title = 'Ayuda Concho'
+		const subject = 'Report'
+		const html = htmlTemplate(body)
 
-	const { data, error } = await resend.emails.send({
-		from: `${title} <${process.env.EMAIL_SENDER}>`,
-		to: process.env.EMAIL_TO_SEND!,
-		subject,
-		html,
-		attachments: !attachments ? null : attachments.map((file: any) => ({
-			filename: file.filename,
-			content: file.content
-		})),
+		const { data, error } = await resend.emails.send({
+			from: `${title} <${process.env.EMAIL_SENDER}>`,
+			to: process.env.EMAIL_TO_SEND!,
+			subject,
+			html,
+			attachments: !attachments ? null : attachments.map((file: any) => ({
+				filename: file.filename,
+				content: file.content
+			})),
+		});
+
+		if (error) return res.status(400).json({ error });
+
+		res.status(200).json({ data });
 	});
-
-	if (error) return res.status(400).json({ error });
-
-	res.status(200).json({ data });
-});
 
 export default router
