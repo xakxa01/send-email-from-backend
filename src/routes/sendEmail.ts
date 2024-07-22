f3326dabd684e4b7ad1d71f730608f406338c7c0
xakxa01 <Carlosxaviergomez@outlook.com>
2024-07-22 13:17:14 -0400

    server is ready

--- src/routes/sendEmail.ts
+++ src/routes/sendEmail.ts
@@ -9,11 +9,46 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 const router = Router()
 
 router.post("/", async (req: Request, res: Response) => {
+	const { errorType, message, selectRoute, attachments } = req.body;
+
+	const htmlTemplate = `
+			<ul> 
+				<li>
+					<h3> 
+						<strong>Error Type:</strong> 
+					</h3>
+
+					${errorType}
+				</li>
+				<li>
+					<h3> 
+						<strong>Message:</strong>
+					</h3>
+
+					${message}
+				 </li>
+
+				${!selectRoute ? `` : `
+					<li>
+						<h3>
+							<strong>Route Selected:</strong>
+						</h3>
+
+						${selectRoute}
+					</li>
+				`}
+			</ul>
+		`;
+
 	const { data, error } = await resend.emails.send({
-		from: "Acme <onboarding@resend.dev>",
+		from: "Ayuda Concho <onboarding@resend.dev>",
 		to: "carlosxaviergomezbarriento@gmail.com",
-		subject: "hello world",
-		html: "<strong>it works!</strong>",
+		subject: 'Report',
+		html: htmlTemplate,
+		attachments: attachments.map((file: any) => ({
+			filename: file.filename,
+			content: file.content
+		})),
 	});
 
 	if (error) return res.status(400).json({ error });
